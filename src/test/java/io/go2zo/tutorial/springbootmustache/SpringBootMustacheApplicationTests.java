d3e5013086d14d4c642382db89b3d15c6ac0572e
Jiho Kim <go2zo.ooo@gmail.com>
2021-03-26 17:04:24 +0900

    First commit

---
+++ src/test/java/io/go2zo/tutorial/springbootmustache/SpringBootMustacheApplicationTests.java
@@ -0,0 +1,13 @@
+package io.go2zo.tutorial.springbootmustache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMustacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
