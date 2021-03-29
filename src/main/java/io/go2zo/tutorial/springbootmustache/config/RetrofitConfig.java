57914e74de2a16f8d468084ef39414483e107b51
Jiho Kim <go2zo.ooo@gmail.com>
2021-03-29 11:01:35 +0900

    Retrofit2 configuration

---
+++ src/main/java/io/go2zo/tutorial/springbootmustache/config/RetrofitConfig.java
@@ -0,0 +1,34 @@
+package io.go2zo.tutorial.springbootmustache.config;
+
+import okhttp3.OkHttpClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class RetrofitConfig {
+
+    private static final String BASE_URL = "http://localhost:8080";
+
+    @Bean
+    public OkHttpClient okHttpClient() {
+        return new OkHttpClient.Builder()
+                .connectTimeout(20, TimeUnit.SECONDS)
+                .writeTimeout(60, TimeUnit.SECONDS)
+                .readTimeout(60, TimeUnit.SECONDS)
+                .build();
+    }
+
+    @Bean
+    public Retrofit retrofit(OkHttpClient client) {
+        String baseURL = BASE_URL + "/api";
+        return new Retrofit.Builder()
+                .baseUrl(baseURL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build();
+    }
+}
