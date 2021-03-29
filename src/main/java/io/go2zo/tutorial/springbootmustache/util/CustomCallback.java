42fd514bb383a01525bd9111ac99b91e5910627c
Jiho Kim <go2zo.ooo@gmail.com>
2021-03-29 13:20:53 +0900

    Added retrofit util classes

---
+++ src/main/java/io/go2zo/tutorial/springbootmustache/util/CustomCallback.java
@@ -0,0 +1,19 @@
+package io.go2zo.tutorial.springbootmustache.util;
+
+import lombok.extern.slf4j.Slf4j;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+@Slf4j
+public abstract class CustomCallback<T> implements Callback<T> {
+    @Override
+    public void onResponse(Call<T> call, Response<T> response) {
+        log.debug("response = " + response);
+    }
+
+    @Override
+    public void onFailure(Call<T> call, Throwable t) {
+        t.printStackTrace();
+    }
+}
---
+++ src/main/java/io/go2zo/tutorial/springbootmustache/util/RequestUtil.java
@@ -0,0 +1,29 @@
+package io.go2zo.tutorial.springbootmustache.util;
+
+import lombok.extern.slf4j.Slf4j;
+import retrofit2.Call;
+import retrofit2.Response;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@Slf4j
+public class RequestUtil {
+
+    public static <T> Optional<T> requestSync(Call<T> call) {
+        try {
+            Response<T> execute = call.execute();
+            log.debug("execute = " + execute);
+            if (execute.isSuccessful()) {
+                return Optional.ofNullable(execute.body());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Optional.empty();
+    }
+
+    public static <T> void requestAsync(Call<T> call, CustomCallback<T> callback) {
+        call.enqueue(callback);
+    }
+}
